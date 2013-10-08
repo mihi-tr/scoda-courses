6ba1e77335a78c979e26b5f4431ebcc6933bde52
Michael Bauer <mihi@lo-res.org>
2013-10-08 12:58:53 +0200

    include course description

--- coursepage.py
+++ coursepage.py
@@ -1,6 +1,7 @@
 import csv,urllib2
 import itertools
 import pystache
+import copy
 
 
 class Overview():
@@ -11,6 +12,7 @@ class Overview():
     self.sort_dim=slice
     self.sort=sort
     self.order=order
+    self.slice=slice
     self.dim_levels=set(
       itertools.ifilter(lambda x: x!='',
       reduce(lambda x,y: x+y,
@@ -20,21 +22,22 @@ class Overview():
       self.dim_levels=sorted([i for i in self.dim_levels], 
         key=lambda x: s.index(x))
 
-  def modules_per_group(self,c):
+  def modules_per_group(self,slice):
+    c=copy.copy(slice) 
     if self.sort: 
        s=map(lambda x: x['tag'],self.sort)
        data=sorted(self.data, key=lambda x: (s.index(x[self.order]), x['Title']))
     else:    
       data=self.data
-    return {'name':c,
-      'modules':[{"link":i['Link'], "title":i["Title"].decode("utf-8")} for i in
-      filter(lambda x: c in x[self.dimension].split(), data)]
-      }
-  
+    c['modules']=[{"link":i['Link'], "title":i["Title"].decode("utf-8")} 
+      for i in filter(lambda x: c['tag'] in x[self.dimension].split(), data)]
+    print c  
+    return c
+    
   def render(self):
-    groups=[self.modules_per_group(i) for i in self.dim_levels]
-    f=open(self.template)
-    return pystache.render(f.read(),{"groups":groups}).encode("utf-8")
+    groups=[self.modules_per_group(i) for i in self.slice]
+    with open(self.template) as f:
+      return pystache.render(f.read(),{"groups":groups}).encode("utf-8")
 
 if __name__=="__main__":
   import sys
